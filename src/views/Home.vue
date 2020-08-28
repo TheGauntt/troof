d9e4cf3064da62cc4ee95d0cfaad3a321daca258
Jesse <jessethegauntt+biz@gmail.com>
2020-08-28 12:50:03 -0700

    Flawless

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Dis Tony C?</h1>
+    <img alt="Da Troof" src="../assets/troof.jpg" />
+    <h1>Abzolutely</h1>
+	<h3>And thank you for washin'</h3>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+};
 </script>
